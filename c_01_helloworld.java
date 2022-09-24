91c84998c9c7fbd069d0675c1e91434dcb1d6c6d
aditya1408 <107838878+aditya1408@users.noreply.github.com>
2022-09-24 13:53:50 +0530

    first java program.
    comment will help you to understand the java code....

---
+++ c_01_helloworld.java
@@ -0,0 +1,25 @@
+//comment 
+//imp.....
+//save the file as name given to class contain main() method as in this code main() method is.
+//in class c_01_helloworld that's why we used c_01_helloworld.java
+//public is used to access the method outside the class.
+//static is used because when the jvm execute the code it will class the main() using class c_01_helloworld without creating object.
+//i.e jvm  ->  c_01_helloworld.main()
+//void is used as main() is not returning nothing.
+// main is method name from which the execution of code is started.
+// String[] args is the command line argument which are pass during exection/interpreting the code.
+//System.out.println() is used to display the parameter pass in it on the monitor.
+
+
+//code.
+import java.lang.*;
+public class c_01_helloworld 
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World");
+  }
+
+
+
+}
